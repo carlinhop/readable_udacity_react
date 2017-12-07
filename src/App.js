7ea14a5f18eb44949c3eca7ffe9fc9994c39e97c
carlinhop <carlinhop@gmail.com>
2017-12-07 09:28:54 +0000

    Returning a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "./Post";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      backend: 'backend-data'
-    }
+      backend: "backend-data"
+    };
   }
 
   componentDidMount() {
     const url = `${process.env.REACT_APP_BACKEND}/categories`;
-    console.log('fetching from url', url);
-    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
-                 credentials: 'include' } )
-      .then( (res) => { return(res.text()) })
-      .then((data) => {
-        this.setState({backend:data});
+    console.log("fetching from url", url);
+    fetch(url, {
+      headers: { Authorization: "whatever-you-want" },
+      credentials: "include"
+    })
+      .then(res => {
+        return res.text();
+      })
+      .then(data => {
+        this.setState({ backend: data });
       });
   }
 
@@ -28,13 +33,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Talking to the backend yields these categories: <br/>
-          {this.state.backend}
-        </p>
+        <div>
+          <Post />
+        </div>
       </div>
     );
   }
---
+++ src/Post.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { Component } from "react";
+
+class Post extends Component {
+  render() {
+    return "hola";
+  }
+}
+
+export default Post;
