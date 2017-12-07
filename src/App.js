0a48e77452bd8333843d7c7f05b44a592dba0ec7
carlinhop <carlinhop@gmail.com>
2017-12-07 09:54:35 +0000

    problems with import statement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Post } from "./components/Post";
+import Post from "./components/Post";
 
 class App extends Component {
   constructor(props) {
