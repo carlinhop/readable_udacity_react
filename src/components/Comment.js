93b9e7049fee661d11bcab69e08501487fe9bb36
carlinop <carlinhop@gmail.com>
2017-12-07 10:13:51 +0000

    Added a Comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Component} from "react"
+
+class Comment extends Component{
+  render(){
+	return (
+      <div>
+      	"This is a comment"
+      </div>
+    )
+  }
+}
+
+export default Comment
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,13 @@
-import react from "react";
+import React from "react";
 import { Component } from "react";
+import Comment from "./Comment"
 
 class Post extends Component {
   render() {
     return (
     <div>
-      "hola"
-    </div>;
+      <Comment/>
+    </div>
     )
   }
 }
