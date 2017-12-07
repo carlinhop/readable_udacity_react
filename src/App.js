2e77a12e6d5b44d56cfa62a5afc353450062cb05
carlinhop <carlinhop@gmail.com>
2017-12-07 09:32:34 +0000

    Returning a component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Post from "./Post";
+import { Post } from "./Post";
 
 class App extends Component {
   constructor(props) {
