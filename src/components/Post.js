3be634cc1d5dae91227a18ffe802e84a349760bd
Carlos Pereira <carlinhop@gmail.com>
2017-12-15 14:13:50 +0000

    Better styling for the post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,13 +5,20 @@ import AddComment from "./AddComment"
 class Post extends Component {
   render() {
     return (
-    <div className="post" flex-direction="row">
-      <button>Vote</button>
-      <h2>Title: Titulo</h2>
-      <p>Minutes ago: </p>
-      <p>hide</p>
-      <p>discuss</p>
-      <p>2 comments</p>
+    <div className="post">
+      <button>Vote Up</button>
+      <button>Vote Down</button>
+      <div className="post-title-details-container">
+        <div className="post-title">
+          <h2>Title: Titulo</h2>
+        </div>
+        <div className="post-details">
+          <p>Minutes ago: </p>
+          <p>hide</p>
+          <p>discuss</p>
+          <p>2 comments</p>
+        </div>
+      </div>
     </div>
     )
   }
