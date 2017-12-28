ab9a3ba9229bd089cf49a373491cb3a093b4d3a4
root <carlinhop@gmail.com>
2017-12-28 22:06:48 +0000

    Added a link to / from the MenuBar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,9 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import IconButton from 'material-ui/IconButton';
 import AppBar from 'material-ui/AppBar';
+
 
 let MenuBar = (props) => {
 
@@ -10,7 +14,10 @@ let MenuBar = (props) => {
       
   	return(
     
-  		<AppBar className="appbar" title="Readable"iconClassNameRight="muidocs-icon-navigation-expand-more" style={props.style}>
+  		<AppBar className="appbar" title="Readable"iconClassNameRight="muidocs-icon-navigation-expand-more" style={props.style}
+      	iconElementLeft={<Link to="/"> <IconButton><NavigationMenu color="white"/></IconButton> </Link>}
+      	>
+      	
     	</AppBar>
      
     )
