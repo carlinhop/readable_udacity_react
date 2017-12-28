d420edabc61af94f5ba14511c88ef39deacf33ce
root <carlinhop@gmail.com>
2017-12-28 21:02:47 +0000

    More css work

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,14 +1,14 @@
 import React from "react"
 import {Component} from "react"
 import TextField from 'material-ui/TextField';
-import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class AddComment extends Component{
   render(){
 	return (
       <div className="add-comment">
 		<TextField multiLine={true}></TextField>
-        <FlatButton label="publish comment"/>
+        <RaisedButton label="publish" primary={true}/>
       </div>
     )
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Component } from "react";
 import {Link} from "react-router-dom";
-import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import {Card, CardActions, CardHeader} from 'material-ui/Card';
 
 class Post extends Component {
   render() {
     const test_id = 1;
     const style = {margin: 12, 'text-decoration': 'none'}
-    
+  
     return (
     <div className="post">
       <Card>
-        <CardHeader
+        <CardHeader 
           title="A Federal Ban on Making Lethal Viruses Is Lifted"
         />
         <CardActions>
-          <FlatButton label="vote up"  primary={true} style={style} />
-          <FlatButton label="vote down" secundary={true} style={style}/>
-          <FlatButton label="hide" />
-          <FlatButton label={<Link to={`/post/${test_id}`} style={style}>comment</Link>}/>
+          
+		  <RaisedButton label="up" style={style}/>
+          <RaisedButton label="down" style={style}/>
+          <RaisedButton label="hide" />
+          <RaisedButton label={<Link to={`/post/${test_id}`} style={style}>comment</Link>}/>
         </CardActions>
       </Card>
     </div>
